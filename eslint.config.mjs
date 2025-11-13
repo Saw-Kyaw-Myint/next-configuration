7fa3b835d309de3240f99cb003e4cb71a1793d3a
Saw Kyaw Myint <sawkyawmyint@metateammyanmar.com>
2025-11-13 08:42:55 +0630

    configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,15 +10,31 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.{js,mjs,cjs,jsx,ts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      'class-methods-use-this': 'warn',
+      curly: 'error',
+      'eol-last': ['warn', 'always'],
+      indent: ['warn', 2],
+      'jsx-a11y/alt-text': 'error',
+      'no-console': ['error', { allow: ['warn', 'error'] }],
+      'no-multiple-empty-lines': ['error', { max: 1 }],
+      'no-trailing-spaces': ['warn'],
+      'no-undef': ['warn'],
+      'no-unused-expressions': ['warn'],
+      'no-unused-vars': 'warn',
+      'no-useless-constructor': 0,
+      'prefer-const': 'error',
+      quotes: ['warn', 'single', { avoidEscape: true }],
+      'react/no-unknown-property': 'error',
+      semi: ['warn', 'always'],
+    },
   },
 ];
 
