2d798703ab50a429c8eea395943dc8ef259a8056
Aung Si Thu <aungsithu@metateammyanmar.com>
2025-11-13 13:39:04 +0630

    [feat/test_precommit] - test precommit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import Image from 'next/image';
 
 export default function Home() {
+  const data = [];
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
+    <div className=" grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <div className="flex justify-center bg-red-500 px-4 text-white"></div>
         <Image
